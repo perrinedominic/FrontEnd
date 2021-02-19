3f209de76b8da1629ff3919667a7480b53d49618
Boderman Smith <16710836@mstc.edu>
2021-02-18 20:28:11 -0600

    Initial commit for MarysToys project

---
+++ MarysToys/Source/js/site.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    showQuote();
+});
+
+// An array holding all of the possible header quotes.
+var possibleQuotes = [
+    'What a great day to buy toys!',
+    'We sell toys - it\'s what we do!',
+    'If you came here to by cookies, you came to the wrong place...',
+    'Oh happy day!',
+    'Mary says Legos are the best!',
+    'foo',
+    'foo',
+    'foo',
+    'foo',
+    'foo'
+];
+
+function showQuote() {
+    // Get a random quote from the quote array.
+    var randomQuote = possibleQuotes[Math.floor(Math.random() * 10)];
+
+    // Put the quote into the quote container element on the page.
+    document.getElementById('quoteContainer').innerHTML = randomQuote;
+
+    alert('test');
+}
