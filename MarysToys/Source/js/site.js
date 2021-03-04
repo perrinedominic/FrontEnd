fce6e0fac4f6fabd7df7022d299255e167384208
Andrew Bliek <17061238@mstc.edu>
2021-03-04 14:01:24 -0600

    added another quote list

--- MarysToys/Source/js/site.js
+++ MarysToys/Source/js/site.js
@@ -16,6 +16,15 @@ var possibleQuotes = [
     'You can never have enough toys!'
 ];
 
+// An array for holding the thank you quotes.
+var thankYouQuotes = [
+    'Thank you for shopping with us!',
+    'We appreciate your business!',
+    'Please recommend us to your friends and family!',
+    'Check back often for offers and special deals!',
+    'Thank you for your purchase!'
+];
+
 // Hide and show descriptions. (P.S after I put these in I realized I couldve just used two arguements. -boden)
 $(function() {
     $("#hideShow").click(function() {
