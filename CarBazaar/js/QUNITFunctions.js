5700dcf4d96c144255b1a6ad2bb932ffd3127a0c
Boderman <16710836@mstc.edu>
2021-04-06 11:59:10 -0500

    Added a functioning quint test.

---
+++ CarBazaar/js/QUNITFunctions.js
@@ -0,0 +1,4 @@
+function IsGreaterThanZero(val)
+{
+   return val > 0;
+}
---
+++ CarBazaar/js/test.js
@@ -0,0 +1,17 @@
+// Tests if valus are greater than zero.
+QUnit.test('IsGreaterThanZero()', function(assert)
+{
+    // arrange
+    var values = [];
+
+    // act
+    values = [0, -4, 5, 12, -3];
+
+    // assert
+    assert.ok(IsGreaterThanZero(values[0]), "0 is not greater than zero.");
+    assert.ok(IsGreaterThanZero(values[1]), "-4 is not greater than zero.");
+    assert.ok(IsGreaterThanZero(values[2]), "5 is greater than zero.");
+    assert.ok(IsGreaterThanZero(values[3]), "12 is greater than zero.");
+    assert.ok(IsGreaterThanZero(values[4]), "-3 is not greater than zero.");
+
+})
