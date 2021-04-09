ef8ef455e9d8fdb442f90853cd7545253810e69e
Boden Smith <16710836@mstc.edu>
2021-04-09 15:14:30 -0500

    Added the js starter files.

---
+++ CarBazaar/js/library.js
@@ -0,0 +1,29 @@
+// Takes an array of words, and concatenates them together to form a sentence.
+function ConcatArrayOfWords(words) {
+
+}
+
+// Takes an array of words, reverses them, and concatenates them together to form a sentence.
+function ConcatAndReverseArrayOfWords(words) {
+
+}
+
+// Checks to see if a word exists within a sentence.
+function CheckForWordInSentence(sentence, word) {
+
+}
+
+// Tests if a number's square root is a whole number.
+function TestIfSquareRootIsWholeNumber(number) {
+
+}
+
+// Returns the sum of an array of numbers passed-in.
+function AddArrayOfNumbers(numbers) {
+
+}
+
+// Gets a random whole number between 0 and the number passed in (maxNumber).
+function GetRandomInteger(maxNumber) {
+
+}
