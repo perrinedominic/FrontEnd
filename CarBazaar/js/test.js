159ecf5a29944fe22141f26f9a42d7d6eefbd502
Andrew Bliek <17061238@mstc.edu>
2021-04-09 22:13:38 -0500

    added QUnit to Add array, nauv92

--- CarBazaar/js/test.js
+++ CarBazaar/js/test.js
@@ -11,3 +11,15 @@ QUnit.test('ConcatArrayOfWords()', function(assert)
     assert.equal(ConcatArrayOfWords(values), "My chicken ate some corn ", 'My chicken ate some corn ');
 
 })
+
+QUnit.test('AddArrayOfNumbers()', function(assert)
+{
+    // Arrange
+    var values = [];
+
+    // Act
+    values = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10];
+
+    // Assert
+    assert.equal(AddArrayOfNumbers(values, 55));
+})
