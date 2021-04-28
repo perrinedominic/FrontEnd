400cbfeaa2e30456256ffa4e448d8fff6c505e62
Dominic Perrine <16063254@mstc.edu>
2021-04-27 20:16:20 -0500

    Included date validation to the search form for nauv-118

--- AceFrontier/js/site.js
+++ AceFrontier/js/site.js
@@ -1,28 +1,36 @@
-function submitContactForm()
-{
+function submitContactForm() {
     let msg = document.getElementById('emailMessage').value;
     let subject = document.getElementById('emailSubject').value;
     let name = document.getElementById('emailName').value;
 
-    if(name != "" && msg != "" && subject != "")
-    {
-        window.location.href = "mailto:AceFrontier@travel.com?subject="+subject+"&body=Hi AceFrontier! My name is "+name +". %0d%0a %0d%0a"+ msg;
-    }
-    else
-    {
+    if (name != "" && msg != "" && subject != "") {
+        window.location.href = "mailto:AceFrontier@travel.com?subject=" + subject + "&body=Hi AceFrontier! My name is " + name + ". %0d%0a %0d%0a" + msg;
+    } else {
         document.getElementById("emailError").textContent = "Please enter all fields.";
     }
 }
 
-function compare()
-{
+function compare() {
     var startDt = document.getElementById("checkIn").value;
     var endDt = document.getElementById("checkOut").value;
 
-    if( (new Date(startDt).getTime() < new Date(endDt).getTime()))
-    {
+    if (startDt > endDt) {
         alert("Check-In date cannot be after the Check-Out date");
     }
+}
 
-    alert(startDt);
+function getDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementsByName("formDates").setAttribute("min", today);
 }
